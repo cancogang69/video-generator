48baccd20dcfbc1b35c94bedb3bd73c38ceaebda
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-07-22 17:03:05 +0700

    update remove path form clip dict

--- configs/opensora-v1-2/inference/sample_with_clip.py
+++ configs/opensora-v1-2/inference/sample_with_clip.py
@@ -28,7 +28,7 @@ vae = dict(
 )
 text_encoder = dict(
     type="clip",
-    path="openai/clip-vit-huge-patch14",
+    # path="openai/clip-vit-huge-patch14",
     model_max_length=100,
 )
 scheduler = dict(
