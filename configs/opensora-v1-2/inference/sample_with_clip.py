8d8c3f12b3169c773ec62ecc37aa7dc1e2ba6f7f
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-07-24 16:01:44 +0700

    change hugging face clip repo path

--- configs/opensora-v1-2/inference/sample_with_clip.py
+++ configs/opensora-v1-2/inference/sample_with_clip.py
@@ -28,7 +28,7 @@ vae = dict(
 )
 text_encoder = dict(
     type="clip",
-    from_pretrained="openai/clip-vit-huge-patch14",
+    from_pretrained="openai/clip-vit-large-patch14",
     model_max_length=100,
 )
 scheduler = dict(
